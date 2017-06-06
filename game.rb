25cb54594f276f51dabe65a6df0e5941bad98238
Newton Ni <cn279@cornell.edu>
2017-06-06 02:51:42 -0400

    Usage prompt

--- game.rb
+++ game.rb
@@ -2,6 +2,11 @@
 require "./game_board"
 require "./game_view"
 
+unless ARGV.length == 3 then 
+    puts "Usage: ruby game.rb <m> <n> <density>" 
+    Kernel.exit(-1)
+end
+
 m, n, density = ARGV
 m = m.to_i
 n = n.to_i
